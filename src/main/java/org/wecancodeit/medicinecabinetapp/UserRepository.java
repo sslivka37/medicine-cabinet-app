7dc78e208dfd20f9cf54bdbb3d1866c1d6bad18b
reneemic <reneemic75@gmail.com>
2019-03-31 16:34:25 -0400

    added user controller and implments

--- src/main/java/org/wecancodeit/medicinecabinetapp/UserRepository.java
+++ src/main/java/org/wecancodeit/medicinecabinetapp/UserRepository.java
@@ -5,4 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 //will have to add parameters for UserRepository(ie, User, Long)
 public interface UserRepository extends CrudRepository<User, Long> {
 
-}
+	  void register(User user);
+	  
+	  User validateUser(Login login);
+	}
+	
+
