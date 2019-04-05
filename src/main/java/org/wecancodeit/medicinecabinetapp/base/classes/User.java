3a59e1c1e6d659abc452449896e4d4c9866d0f9c
anicgarr <anicgarr@aol.com>
2019-04-04 22:23:53 -0400

    4.4

--- src/main/java/org/wecancodeit/medicinecabinetapp/base/classes/User.java
+++ src/main/java/org/wecancodeit/medicinecabinetapp/base/classes/User.java
@@ -1,9 +1,11 @@
 package org.wecancodeit.medicinecabinetapp.base.classes;
 
 import javax.annotation.Generated;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class User {
 	
 	@Id
