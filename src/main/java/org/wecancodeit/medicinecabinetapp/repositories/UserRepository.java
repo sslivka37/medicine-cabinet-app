095050a9b3a45936bc568075fea0d77cc12657ca
reneemic <reneemic75@gmail.com>
2019-04-07 19:50:35 -0400

    working through displaying meds

--- src/main/java/org/wecancodeit/medicinecabinetapp/repositories/UserRepository.java
+++ src/main/java/org/wecancodeit/medicinecabinetapp/repositories/UserRepository.java
@@ -1,9 +1,6 @@
 package org.wecancodeit.medicinecabinetapp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import org.wecancodeit.medicinecabinetapp.Login;
 import org.wecancodeit.medicinecabinetapp.base.classes.User;
 
@@ -16,21 +13,5 @@ public interface UserRepository extends CrudRepository<User, Long> {
 	}
 	
 
-=======
-=======
->>>>>>> development
-=======
->>>>>>> adrienne
-import org.wecancodeit.medicinecabinetapp.base.classes.User;
 
-//assuming we are using generated ID values and a similar set up to reviews site, parameters will be User, Long
-public interface UserRepository extends CrudRepository<User, Long> {
 
-}
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> development
-=======
->>>>>>> development
-=======
->>>>>>> adrienne
