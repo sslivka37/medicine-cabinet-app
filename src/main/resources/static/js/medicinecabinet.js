6a49eff27d4e119e7539a597bd914af16ba77d1e
Your Name <you@example.com>
2019-04-14 16:48:32 -0400

    Sarah commit, working through javascript, waiting for Ernest to see if we can ditch Thymeleaf

--- src/main/resources/static/js/medicinecabinet.js
+++ src/main/resources/static/js/medicinecabinet.js
@@ -16,15 +16,49 @@ window.onclick = function(event) {
 }
 
 //function to show daily medications in medication for today template
-var listOfMedications = [[${medications}]]
-var medicationToShowToday = document.getElementById("eachMedicationForToday");
-var medicationFrequency = document.getElementById("frequencyCheck");
+//var listOfMedications = [[${medications}]]
+//var medicationToShowToday = document.getElementById("eachMedicationForToday");
+//var medicationFrequency = document.getElementById("frequencyCheck");
 
-function showDaily(){
-	for(i = 0; i<listOfMedications.length; i++){
-	if (medicationFrequency === "daily" || medicationFrequency === "Every 4 Hours" || medicationFrequency ==="Every 8 Hours" || medicationFrequency ==="Every 12 Hours"){
+function showIfDaily(){
+	
+	if (medicationFrequency === "daily"){
 		medicationToShowToday.style.display ="block";
 	} else {
 		medicationToShowToday.style.display ="none";
 	}
-}}
+}
+
+
+function showIfEveryFourHours(){
+	if (medicationFrequency ==="Every 4 Hours"){
+		medicationToShowToday.style.display = "block";
+	}else{
+		medicationToShowToday.style.display = "none";
+	}
+}
+
+
+function showIfEveryEightHours(){
+	if(medicationFrequency === "Every 8 Hours"){
+		medicationToShowToday.style.display = "block";
+	} else {
+		medicationToShowToday.style.display = "none";
+	}
+}
+
+function showIfEveryTwelveHours(){
+	if(medicationFrequency === "Every 12 Hours"){
+		medicationToShowToday.style.display = "block";
+	} else {
+		medicationToShowToday.style.display = "none";
+	}
+}
+
+//functions for weekly, build out one for every day?
+
+//if mediction frequency is weekly- monday, and today is monday, show medication......tues, weds, thurs, fri, sat, sun...
+
+//function showIfDayOfWeekMatchesToday(){
+//	if(medicationFrequency ===)
+//}
