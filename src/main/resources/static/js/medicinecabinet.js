734d3e252990ad0d3d1ba3c17c0e378eaa50f7e6
Ernest Bluford <e.daniel.bluford@outlook.com>
2019-04-19 07:40:50 -0400

    Solved js issue

--- src/main/resources/static/js/medicinecabinet.js
+++ src/main/resources/static/js/medicinecabinet.js
@@ -1,70 +1,57 @@
-function myFunction() {
-	document.getElementById("myDropdown").classList.toggle("show");
+var medications = document.getElementsByClassName("meds");
+var timeTakeMedication = medications.getElementById("timeCheck");
+//  var listOfMedications = ['[$[medications]'];
 
+//     var medicationToShowToday = document.getElementById("eachMedicationForToday");
+//     var medicationFrequency = document.getElementById("frequencyCheck");
 
-}
-
-window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
-		var dropdowns = document.getElementsByClassName("dropdown-content");
-		var i;
-		for (i = 0; i < dropdowns.length; i++){
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
-}
-
- var listOfMedications = [[${medications}]];
-    var medicationToShowToday = document.getElementById("eachMedicationForToday");
-    var medicationFrequency = document.getElementById("frequencyCheck");
-
-    medicationFrequency.
-
-
-
+//     medicationFrequency.
 
 //function to show daily medications in medication for today template
 //var listOfMedications = [[${medications}]]
 //var medicationToShowToday = document.getElementById("eachMedicationForToday");
 //var medicationFrequency = document.getElementById("frequencyCheck");
 
-function showIfDaily(){
-	
-	if (medicationFrequency.innerText(); === "daily"){
-		medicationToShowToday.style.display ="block";
-		alert(test);
-	} else {
-		medicationToShowToday.style.display ="none";
-	}
+function isToday() {
+  var i;
+  for (i = 0; i < medications.length; i++) {
+    if (timeTakeMedication.innerText.contains("show")) {
+      openDropdown.classList.remove("show");
+    }
+  }
 }
 
-
-function showIfEveryFourHours(){
-	if (medicationFrequency.innerText() ==="Every 4 Hours"){
-		medicationToShowToday.style.display = "block";
-	}else{
-		medicationToShowToday.style.display = "none";
-	}
+function showIfDaily() {
+  if (medicationFrequency.innerText() === "daily") {
+    medicationToShowToday.style.display = "block";
+    alert(test);
+  } else {
+    medicationToShowToday.style.display = "none";
+  }
 }
 
-
-function showIfEveryEightHours(){
-	if(medicationFrequency === "Every 8 Hours"){
-		medicationToShowToday.style.display = "block";
-	} else {
-		medicationToShowToday.style.display = "none";
-	}
+function showIfEveryFourHours() {
+  if (medicationFrequency.innerText() === "Every 4 Hours") {
+    medicationToShowToday.style.display = "block";
+  } else {
+    medicationToShowToday.style.display = "none";
+  }
 }
 
-function showIfEveryTwelveHours(){
-	if(medicationFrequency === "Every 12 Hours"){
-		medicationToShowToday.style.display = "block";
-	} else {
-		medicationToShowToday.style.display = "none";
-	}
+function showIfEveryEightHours() {
+  if (medicationFrequency === "Every 8 Hours") {
+    medicationToShowToday.style.display = "block";
+  } else {
+    medicationToShowToday.style.display = "none";
+  }
+}
+
+function showIfEveryTwelveHours() {
+  if (medicationFrequency === "Every 12 Hours") {
+    medicationToShowToday.style.display = "block";
+  } else {
+    medicationToShowToday.style.display = "none";
+  }
 }
 
 //functions for weekly, build out one for every day?
